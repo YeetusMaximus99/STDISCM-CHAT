ef5bbeb9682be50afb13932c7d93afe08d2fef4f
TedrickHernandez <tedrick_hernandez@dlsu.edu.ph>
2024-04-12 14:42:36 +0800

    Changed MySQL password back to 0000. Oops!

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ def listener(cs,client_sockets):
             # and send the message
             db = mysql.connector.connect(host="localhost",    # your host, usually localhost
                      user="root",         # your username
-                     passwd="MoonbeaN!?0645",  # your password
+                     passwd="0000",  # your password
                      db="new_schema")
             mycursor = db.cursor()
             sql= "INSERT INTO messages (user,messagescol) VALUES (%s,%s)"
