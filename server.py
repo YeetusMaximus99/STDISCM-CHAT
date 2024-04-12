6a4b24a02be42bbaa41c0ff3018526679855174d
TedrickHernandez <tedrick_hernandez@dlsu.edu.ph>
2024-04-12 14:46:11 +0800

    (1) Handle message distribution and database interaction more cleanly; (2) ensure sockets close properly on client disconnects.

--- server.py
+++ server.py
@@ -1,51 +1,51 @@
 import socket
 from threading import Thread
 import mysql.connector
-def listener(cs,client_sockets):
+
+def listener(cs, client_sockets):
     while True:
         try:
             # Listen for client message
             msg = cs.recv(1024).decode()
+            if not msg:
+                raise Exception("Client disconnected.")
         except Exception as e:
             print(e)
             #remove faulty client
             client_sockets.remove(cs)
-        # iterate over all connected sockets
-        for client_socket in client_sockets:
-            # and send the message
-            db = mysql.connector.connect(host="localhost",    # your host, usually localhost
-                     user="root",         # your username
-                     passwd="0000",  # your password
-                     db="new_schema")
-            mycursor = db.cursor()
-            sql= "INSERT INTO messages (user,messagescol) VALUES (%s,%s)"
-            val = ("test",msg.encode())
-            mycursor.execute(sql, val)
-            db.commit()
-            client_socket.send(msg.encode())
-            
+            cs.close()
+            return
+        # iterate over all connected sockets and distribute message
+        distribute_message(msg, client_sockets)
+
+def distribute_message(msg, client_sockets):
+    db = mysql.connector.connect(host="localhost", user="root", passwd="0000", db="new_schema")
+    cursor = db.cursor()
+    sql = "INSERT INTO messages (user, messagescol) VALUES (%s, %s)"
+    user, content = msg.split(":", 1)  # Assume messages are in "name:message" format
+    cursor.execute(sql, (user, content))
+    db.commit()
+    cursor.close()
+    db.close()
+    for client_socket in client_sockets:
+        client_socket.send(msg.encode())
+
 host = input("Enter Host Address:")
-port =int(input("Enter Port Used:"))
+port = int(input("Enter Port Used:"))
 
 x = 5
 client_sockets = set()
 s = socket.socket()
-#Port is made reuseable
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#bind port to address
 s.bind((host, port))
-#amount of clients that can listen
 s.listen(x)
 print(f"[*] Listening as {host}:{port}")
 
-
 while True:
     client_socket, client_address = s.accept()
-    print(f"[+] {client_address} is here!")
-    # add client
+    print(f"[+] {client_address} connected.")
     client_sockets.add(client_socket)
-    # Thread to listen for client
-    t = Thread(target=listener, args=(client_socket,client_sockets))
+    t = Thread(target=listener, args=(client_socket, client_sockets))
     t.daemon = True
     t.start()
 
